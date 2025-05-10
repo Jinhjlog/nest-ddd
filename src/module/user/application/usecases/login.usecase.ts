fe8ed5f2dce862b5d2f3b5716ab63db8b2da590f
Jinhjlog <dndn2615@gmail.com>
2025-05-10 22:30:21 +0900

    temp

--- src/module/user/application/usecases/login.usecase.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthService } from 'src/module/core/auth/auth.service';
-
-@Injectable()
-export class LoginUseCase {
-  constructor(private readonly authService: AuthService) {}
-
-  execute() {}
-}
--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -1,14 +1,11 @@
 import { Module, Provider } from '@nestjs/common';
-import { UserController } from './presentation/controllers/user.controller';
-import { CreateUserUseCase, LoginUseCase } from './application/usecases';
+import { CreateUserUseCase } from './application/usecases';
 import { UserRepository } from './domain/repositories';
 import { UserRepositoryImpl } from './infra/repositories';
-import { AuthController } from './presentation/controllers/auth.controller';
 
-const usecases: Provider[] = [CreateUserUseCase, LoginUseCase];
+const usecases: Provider[] = [CreateUserUseCase];
 
 @Module({
-  controllers: [AuthController, UserController],
   providers: [
     ...usecases,
     {
